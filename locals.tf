dacfd9eadea16829f9321bfbf8659c5dffd411b8
Illia Lobachov <142513292+Anti4ris98@users.noreply.github.com>
2025-09-07 19:22:11 +0200

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -2,24 +2,24 @@ locals {
   vpc_cidr = "100.64.0.0/16"
   public_subnets = [
     { cidr_block        = "100.64.0.0/20"
-      availability_zone = "eu-central-1a"
+      availability_zone = "eu-west-2a"
     },
     { cidr_block        = "100.64.32.0/20"
-      availability_zone = "eu-central-1b"
+      availability_zone = "eu-west-2b"
     },
     { cidr_block        = "100.64.64.0/20"
-      availability_zone = "eu-central-1c"
+      availability_zone = "eu-west-2c"
     },
   ]
   private_subnets = [
     { cidr_block        = "100.64.16.0/20"
-      availability_zone = "eu-central-1a"
+      availability_zone = "eu-west-2a"
     },
     { cidr_block        = "100.64.48.0/20"
-      availability_zone = "eu-central-1b"
+      availability_zone = "eu-west-2b"
     },
     { cidr_block        = "100.64.80.0/20"
-      availability_zone = "eu-central-1c"
+      availability_zone = "eu-west-2c"
     },
   ]
   ssh_key_name = "django"
